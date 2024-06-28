c54f7e3040f15c4a82d8ec6295754245f9a81d60
marlone000 <142364606+marlone000@users.noreply.github.com>
2024-06-28 00:13:46 -0300

    v

---
+++ Footer.php
@@ -0,0 +1,40 @@
+<!-- footer inicio -->
+<div id="footer_content" class="footer-letter">
+    <div id="footer_contacts">
+        <h1>Anime Code</h1>
+        <p>Nós somos nossos sonhos!</p>
+    </div>
+    &nbsp;
+    <ul class="footer-list">
+        <li>
+            <h3>Blog</h3>
+        </li> 
+        <li>
+            <a href="#" class="footer-link">Animes</a>
+        </li>
+    </ul>
+    <ul class="footer-list">
+        <li>
+            <h3>Sobre</h3>
+        </li>
+       
+        <li>
+            <a href="#" class="footer-link">Quem Somos</a>
+        </li>
+
+    </ul>
+
+    <img src="/IMG/logo_senac.png" alt="" class="img-fluid"> <a class="logo" href="#"></a>
+    <br><br>
+</div>
+
+<div id="footer_copyright">
+    <p>&#169
+        Anime Code, T.I 22 all rights reserved</p>
+    
+</div>
+
+  <!-- footer fim -->
+      <script src="/JAVASCRIPT/global.js"></script>
+</body>
+</html>
